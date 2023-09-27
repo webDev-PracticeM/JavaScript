e71c754841532d6e34fd70adbe421d5d16cfcc1f
stephanie <sclie001@fiu.edu>
2023-09-26 22:29:55 -0400

    only display number buttons when clicked

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const inputBtns = document.querySelectorAll('button');
+const calculator_display = document.querySelector('h1');
+
+function displayValue(btn_value){
+    calculator_display.textContent = btn_value;
+}
+inputBtns.forEach((inputBtn) =>{
+    if(Number(inputBtn.value) >= 0 && Number(inputBtn.value) <= 9){
+        inputBtn.addEventListener('click', () => displayValue(inputBtn.value));
+    }
+    
+});
