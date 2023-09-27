031881a89446186f2804092dbc8ecb6e73e7d315
stephanie <sclie001@fiu.edu>
2023-09-26 22:42:24 -0400

    changed argument var name for displayValue func

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,9 +1,11 @@
 const inputBtns = document.querySelectorAll('button');
 const calculator_display = document.querySelector('h1');
 
-function displayValue(btn_value){
-    calculator_display.textContent = btn_value;
+/** Display the value*/
+function displayValue(a_value){
+    calculator_display.textContent = a_value;
 }
+
 inputBtns.forEach((inputBtn) =>{
     if(Number(inputBtn.value) >= 0 && Number(inputBtn.value) <= 9){
         inputBtn.addEventListener('click', () => displayValue(inputBtn.value));
