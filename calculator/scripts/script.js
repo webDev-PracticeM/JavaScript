a7937cd645ffcb0d92aad4e0634af3874333925c
stephanie <sclie001@fiu.edu>
2023-10-04 16:52:00 -0400

    concatenate display string with number btns pressed by user

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -3,7 +3,12 @@ const calculator_display = document.querySelector('h1');
 
 /** Display the value*/
 function displayValue(a_value){
-    calculator_display.textContent = a_value;
+    if(calculator_display.textContent === '0'){
+        calculator_display.textContent = a_value;
+    }
+    else{
+        calculator_display.textContent += a_value;
+    }
 }
 
 inputBtns.forEach((inputBtn) =>{
