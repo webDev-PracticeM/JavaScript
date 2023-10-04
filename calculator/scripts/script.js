6dab5e910c9f8e7be48aaab51588106529c5246b
stephanie <sclie001@fiu.edu>
2023-10-04 17:46:48 -0400

    added functionality to check if an operator was pressed

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,6 +1,11 @@
 const inputBtns = document.querySelectorAll('button');
 const calculator_display = document.querySelector('h1');
 
+let first_operand = 0;
+let second_operand = 0;
+let operator_pressed = false;//flag for operator being pressed
+
+
 /** Display the value*/
 function displayValue(a_value){
     if(calculator_display.textContent === '0'){
@@ -11,9 +16,32 @@ function displayValue(a_value){
     }
 }
 
+function isOperatorPressed(){
+    if(operator_pressed){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+/**Determine which operator is being pressed */
+function checkOperator(a_operator){
+    isOperatorPressed();
+    console.log(a_operator);
+
+}
+
+//TODO: assign first operand (make sure first operand's value is 0)
+//TODO: assign second operand, but make sure first operand != 0 && second operand == 0
+//TODO: calculate
+
 inputBtns.forEach((inputBtn) =>{
     if(Number(inputBtn.value) >= 0 && Number(inputBtn.value) <= 9){
         inputBtn.addEventListener('click', () => displayValue(inputBtn.value));
+    }
+    else if (inputBtn.className === 'operator'){
+        inputBtn.addEventListener('click', () => checkOperator(inputBtn.value));
     }
     
 });
