a3095a84a7d87db6d38a1a4829130a20e47c4d3a
sclie001 <daisysc7789@gmail.com>
2024-02-23 13:44:59 -0500

    selected a quote and it's author from fetched quotes from API

--- quote-generator/scripts/script.js
+++ quote-generator/scripts/script.js
@@ -1,5 +1,4 @@
 const newQuoteBtn = document.getElementById('quote-btn');
-
 let apiQuotes = [];
 
 /**Get quotes from API */
@@ -9,13 +8,28 @@ async function getQuotes(){
 
     try {
         const response = await fetch(apiUrl);
-        apiQuotes = response.json();
-        console.log(apiQuotes);
+        apiQuotes = await response.json();
+        newQuote();
+
     } catch (error) {
         console.log("Failed to fetch quote", error);
         //create another page to present user we are working diligently to resolve this issue.
     }
 }
 
+/**A new quote is selected among fetched quotes from API */
+function newQuote(){
+
+    //pick a random quote 
+   let index = Math.floor(Math.random() * apiQuotes.length);
+   let quote = apiQuotes[index].text;
+   let author = apiQuotes[index].author;
+   
+
+
+}
+
 //Event listener
-newQuoteBtn.addEventListener('click', getQuotes);
+newQuoteBtn.addEventListener('click', newQuote);
+
+getQuotes();
